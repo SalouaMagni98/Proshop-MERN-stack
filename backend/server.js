fcae674bbeb0d3eb43ec81488b8d8e10e9455cc0
SalouaMagni98 <salouamagni@gmail.com>
2024-03-20 01:53:06 +0100

    Creat Models

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import products from './data/products.js';
-dotenv.config();    
+dotenv.config();
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
 
 const port = process.env.PORT || 5000;
+connectDB();  //Connect to MOngoDb
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.get('/api/products', (req, res) => { 
+app.get('/api/products', (req, res) => {
     res.json(products);
-}); 
+});
 
-app.get('/api/products/:id', (req, res) => {  
+app.get('/api/products/:id', (req, res) => {
     const product = products.find((p) => p._id === req.params.id);
     res.json(product);
 });
