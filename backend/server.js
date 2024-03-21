d931e88ac6f4e3e9b475ebd935df5ba75b94f3b0
SalouaMagni98 <salouamagni@gmail.com>
2024-03-21 03:37:31 +0100

    Products From Database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import products from './data/products.js';
 dotenv.config();
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-
+import productRoute from './routes/productRoute.js';
 
 const port = process.env.PORT || 5000;
 connectDB();  //Connect to MOngoDb
@@ -13,14 +12,7 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoute); 
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
